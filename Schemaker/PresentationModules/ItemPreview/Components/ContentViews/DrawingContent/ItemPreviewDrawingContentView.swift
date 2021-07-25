99a6b2c551e8695313e85ee5adf25fd24a213ec1
Igor Naumenko <igor.naum.2013@yandex.ru>
2021-07-25 23:00:28 +0300

    drawing content ui

--- Schemaker/PresentationModules/ItemPreview/Components/ContentViews/DrawingContent/ItemPreviewDrawingContentView.swift
+++ Schemaker/PresentationModules/ItemPreview/Components/ContentViews/DrawingContent/ItemPreviewDrawingContentView.swift
@@ -11,6 +11,15 @@ final class ItemPreviewDrawingContentView: CommonContentView {
     
     private var lineLayers: [[CGPoint]] = []
     
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
--- Schemaker/PresentationModules/ItemPreview/ItemPreviewViewController.swift
+++ Schemaker/PresentationModules/ItemPreview/ItemPreviewViewController.swift
@@ -24,12 +24,32 @@ final class ItemPreviewViewController: UIViewController {
         super.viewDidLoad()
         configureUI()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        contentView.layer.addShadow(radius: 16, rect: contentView.bounds)
+    }
 }
 
 extension ItemPreviewViewController {
     private func configureUI() {
-        contentView.frame.size = CGSize(width: view.frame.width - 32, height: view.frame.height - 150)
-        contentView.center = view.center
+        configureBlur()
+        configureContent()
+    }
+    
+    private func configureBlur() {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.systemUltraThinMaterialLight)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        view.addSubview(blurEffectView)
+    }
+    
+    private func configureContent() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(contentView)
+        contentView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        contentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -200).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
     }
 }
